01b6db4315bc7632a496d6430330b002d2d249e2
capinho <papadiawara@hotmail.fr>
2020-05-24 01:16:02 +0000

    Prise en charge heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -151,3 +152,5 @@ TEMPLATED_EMAIL_FROM_EMAIL = 'TEST'
 
 
 TEMPLATED_EMAIL_BACKEND = 'templated_email.backends.vanilla_django'
+
+django_heroku.settings(locals())
