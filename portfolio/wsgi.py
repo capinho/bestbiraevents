1398f17de08f7c4882db143bedf230755f258cb9
capinho <papadiawara@hotmail.fr>
2020-05-24 00:53:04 +0000

    Gestion fichiers Static

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='static/')
+application.add_files('static/', prefix='more-files/')
+
